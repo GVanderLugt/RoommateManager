14b3af5b2772dc2bd11b871e9c7de9fac463bac3
Gerrit VanderLugt <gerrit.vanderlugt@gmail.com>
2014-06-21 16:08:23 -0400

    More cleaning up

--- app/src/main/java/edu/iupui/gdvander/roommatemanager/app/NavigationDrawerFragment.java
+++ app/src/main/java/edu/iupui/gdvander/roommatemanager/app/NavigationDrawerFragment.java
@@ -105,6 +105,7 @@ public class NavigationDrawerFragment extends Fragment {
                         getString(R.string.title_section1),
                         getString(R.string.title_section2),
                         getString(R.string.title_section3),
+                        getString(R.string.title_section4)
                 }));
         mDrawerListView.setItemChecked(mCurrentSelectedPosition, true);
         return mDrawerListView;
@@ -244,11 +245,6 @@ public class NavigationDrawerFragment extends Fragment {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         if (mDrawerToggle.onOptionsItemSelected(item)) {
-            return true;
-        }
-
-        if (item.getItemId() == R.id.action_example) {
-            Toast.makeText(getActivity(), "Example action.", Toast.LENGTH_SHORT).show();
             return true;
         }
 
